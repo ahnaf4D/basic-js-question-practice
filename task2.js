efbbb1aba148c85b9c39e26c8bba10272ff42765
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-05 16:10:17 +0600

    Task 2  completed

---
+++ task2.js
@@ -0,0 +1,9 @@
+const Mathematics = 75.25;
+const Biology = 65;
+const Chemistry = 80;
+const Physics = 35.45;
+const Bangla = 99.50;
+
+const totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+const averageMark = totalMarks / 5;
+console.log(`Your average marks is ${averageMark}`);
