69535414a19b6e0c9c368ea3e3d951dfa65dac8a
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-05 16:03:33 +0600

    Task 1 completed

---
+++ task1.js
@@ -0,0 +1,6 @@
+const myMoney = 700;
+const orangePrice = 300;
+const applePrice = 320;
+const totalCost = orangePrice + applePrice;
+const returnMoney = myMoney - totalCost;
+console.log(`Shopkeeper will return me ${returnMoney} BDT`);
